59b95f88b2ad3c5ac8ce817eec76a82353d33a23
Shazim Rahman <shazimrahman@hotmail.com>
2026-01-19 21:17:00 -0500

    added a static webpage to test routing

--- cmd/httpserver-router/main.go
+++ cmd/httpserver-router/main.go
@@ -1,9 +1,12 @@
-package httpserverrouter
+package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 
 	"github.com/ShazimR/tcp-http-server/internal/request"
@@ -14,19 +17,110 @@ import (
 
 const port = 8080
 
+type TestResponse struct {
+	Message   string `json:"msg"`
+	Timestamp uint64 `json:"ts"`
+}
+
+// Helper functions
+func load(filename string) ([]byte, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func serveStatic(filename string, contentType string, w *response.Writer) error {
+	status := response.StatusOK
+	h := response.GetDefaultHeaders(0)
+	h.Replace("Content-Type", contentType)
+	body, err := load(filename)
+	if err != nil {
+		status = response.StatusInternalServerError
+		body = []byte("error loading file")
+		h.Replace("Content-Type", "text/plain")
+	}
+	h.Replace("Content-Length", strconv.Itoa(len(body)))
+
+	err = w.WriteResponse(status, h, body)
+	return err
+}
+
 // Example route handlers
-func rootGetHandler(w *response.Writer, req *request.Request) error  { return nil }
-func rootPostHandler(w *response.Writer, req *request.Request) error { return nil }
-func chunkGetHandler(w *response.Writer, req *request.Request) error { return nil }
-func videoGetHandler(w *response.Writer, req *request.Request) error { return nil }
+func serveIndex(w *response.Writer, req *request.Request) error {
+	err := serveStatic("./static/index.html", "text/html", w)
+	return err
+}
+
+func serveFavicon(w *response.Writer, req *request.Request) error {
+	err := serveStatic("./static/favicon.ico", "image/x-icon", w)
+	return err
+}
+
+func serveStyles(w *response.Writer, req *request.Request) error {
+	err := serveStatic("./static/styles.css", "text/css", w)
+	return err
+}
+
+func serveApp(w *response.Writer, req *request.Request) error {
+	err := serveStatic("./static/app.js", "text/javascript", w)
+	return err
+}
+
+func echo(w *response.Writer, req *request.Request) error {
+	status := response.StatusOK
+	h := response.GetDefaultHeaders(0)
+	h.Replace("Content-Type", "application/json")
+
+	var reqBody TestResponse
+	reqBodyStr := ""
+	method := req.RequestLine.Method
+	path := req.RequestLine.RequestTarget
+	err := json.Unmarshal(req.Body, &reqBody)
+	if err != nil {
+		reqBodyStr = "failed to parse body or body was empty"
+	} else {
+		reqBodyStr = fmt.Sprintf("msg: %s | ts: %d", reqBody.Message, reqBody.Timestamp)
+	}
+
+	fmt.Printf("Method:  %s\n", method)
+	fmt.Printf("Path:    %s\n", path)
+	fmt.Printf("Body:    %s\n", reqBodyStr)
+	fmt.Printf("RawBody: %s\n\n", req.Body)
+
+	body := map[string]string{
+		"Method": method,
+		"Path":   path,
+		"Body":   reqBodyStr,
+	}
+
+	resBody, err := json.Marshal(body)
+	if err != nil {
+		status = response.StatusInternalServerError
+		resBody = []byte("failed to jsonify output")
+		h.Replace("Content-Type", "text/plain")
+	}
+
+	h.Replace("Content-Length", strconv.Itoa(len(resBody)))
+	err = w.WriteResponse(status, h, resBody)
+	return err
+}
 
 func main() {
 	// Set route handlers
 	r := router.NewRouter()
-	r.GET("/", rootGetHandler)
-	r.POST("/", rootPostHandler)
-	r.GET("/chunk", chunkGetHandler)
-	r.GET("/video", videoGetHandler)
+	r.GET("/", serveIndex)
+	r.GET("/index.html", serveIndex)
+	r.GET("/favicon.ico", serveFavicon)
+	r.GET("/styles.css", serveStyles)
+	r.GET("/app.js", serveApp)
+
+	r.GET("/api/echo", echo)
+	r.POST("/api/echo", echo)
+	r.PUT("/api/echo", echo)
+	r.DELETE("/api/echo", echo)
+	r.PATCH("/api/echo", echo)
 
 	// Setup and run server
 	s, err := server.Serve(port, nil, r)
